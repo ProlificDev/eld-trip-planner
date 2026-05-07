cb7ea15f61ea5367aae1a662b257070c96703733
Your Name <your.email@example.com>
2026-05-07 19:23:23 +0100

    Switch map to light/white tiles

--- frontend/src/components/RouteMap.jsx
+++ frontend/src/components/RouteMap.jsx
@@ -65,7 +65,7 @@ function RouteMap({ tripPlan }) {
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
         />
         
         {routeCoords.length > 0 && (
